bb8052e07f36526484b2d9125fafcf8d99339909
Ida Funk <Ida-Funk@users.noreply.github.com>
2025-11-10 23:44:15 +0100

    added gitignore file

--- Advent_of_Code_2024/obj/Debug/net8.0/Advent_of_Code_2024.AssemblyInfo.cs
+++ Advent_of_Code_2024/obj/Debug/net8.0/Advent_of_Code_2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Advent_of_Code_2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+887fcda9123e296f1edd80c537166f080ae484f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Advent_of_Code_2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Advent_of_Code_2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
